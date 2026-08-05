c6c02ae60f49a44e65d5c67a5590cce358200f78
rin-lmm <SLINX.24@ichat.sp.edu.sg>
2026-08-05 12:38:52 +0800

    Update Bullet.cs

--- Assets/Weapons/Scripts/Bullet.cs
+++ Assets/Weapons/Scripts/Bullet.cs
@@ -1,20 +1,33 @@
 using UnityEngine;
 
+// This script handles bullet collision and damage
 public class Bullet : MonoBehaviour
 {
+    // Amount of damage dealt by this bullet
     public int damage = 20;
 
+    // Destroy the bullet automatically after a few seconds
     public float destroyAfter = 5f;
 
     void Start()
     {
+        // Prevent bullets from staying in the scene forever
         Destroy(gameObject, destroyAfter);
     }
 
+    // Called when the bullet collides with another object
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Hit " + collision.gameObject.name);
+        // Try to get the BasicEnemyController script from the object that was hit
+        BasicEnemyController enemy = collision.gameObject.GetComponent<BasicEnemyController>();
 
+        // If the object has the enemy script, deal damage
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+
+        // Destroy the bullet after hitting anything
         Destroy(gameObject);
     }
 }
